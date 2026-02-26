cbbe33704eb3718b2bf060e1a1d148961335005b
ManmeetBhogal <manmeetbhogal99@gmail.com>
2026-02-25 23:58:00 -0500

    TeamMember React Component
    Created TeamMember.tsx to create TeamMember react component with member name's roles, images, and descriptions.

--- app/ui/description.tsx
+++ app/ui/description.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import React from "react";
-import PlasmaBackground from "@/components/ui/plasmaBackground";
 import Image from 'next/image';
+import PlasmaBackground from "@/components/ui/plasmaBackground";
+import TeamSection from "@/components/TeamMember";
 
 export function Description() {
   return (
-    <div className="h-[40rem] w-full bg-[#060010] relative overflow-hidden flex justify-center">
+    <div className="w-full bg-[#060010] relative overflow-hidden flex justify-center">
       <div
         className="absolute inset-0 z-10 pointer-events-none"
         style={{
@@ -20,46 +21,7 @@ export function Description() {
       </div>
 
       <div className="description-container">
-        <h2 className="lg:text-3xl font-bold text-center text-white mb-4">
-          Meet the team
-        </h2>
-
-        <div className="team-member">
-            <Image
-              src="/placeholder.jpg"
-              alt="Dr. Hilary Marusak"
-              width={150}
-              height={150}
-              className="rounded-full mx-auto mb-4"
-            />
-            <h3 className="text-xl font-semibold text-center text-white">Dr. Hilary Marusak</h3>
-            <p className="text-center text-white mt-2">
-              Host
-            </p>
-            <p>Dr. Marusak is a neuroscientist at Wayne State University, and directs the WSU THINK lab. 
-               The THINK lab studies brain development in children and adolescents and the impacts of 
-               environmental stress on the brain, as well as anxiety & PTSD. Dr. Marusak is interested 
-               in neuroscience communication and science-advocacy, and is the host of the brainSTEM podcast.</p>
-        </div>
-
-         <div className="team-member">
-            <Image
-              src="/placeholder.jpg"
-              alt="Dr. Hilary Marusak"
-              width={150}
-              height={150}
-              className="rounded-full mx-auto mb-4"
-            />
-            <h3 className="text-xl font-semibold text-center text-white">Dr. Hilary Marusak</h3>
-            <p className="text-center text-white mt-2">
-              Host
-            </p>
-            <p>Dr. Marusak is a neuroscientist at Wayne State University, and directs the WSU THINK lab. 
-               The THINK lab studies brain development in children and adolescents and the impacts of 
-               environmental stress on the brain, as well as anxiety & PTSD. Dr. Marusak is interested 
-               in neuroscience communication and science-advocacy, and is the host of the brainSTEM podcast.</p>
-        </div>
-
+        <TeamSection />
       </div>
 
       {/* <h2 className="text-center">
