d84431ff489a74f3d921c0ba070a29965637d4d2
manmeetbhogal <manmeetbhogal99@gmail.com>
2026-04-22 00:02:55 -0400

    feature: add Umami analytics via proxied script in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 // import { Geist, Geist_Mono } from "next/font/google";
 import { Figtree } from "next/font/google";
 import '@/app/ui/globals.css';
+import Script from "next/script";
 
 const figtree = Figtree({
   subsets: ["latin"],
@@ -34,9 +35,17 @@ export default function RootLayout({
   return (
     <html lang="en" style={{ position: "relative" }}>
       <body
-        className={`${figtree.variable} ${figtree.variable} antialiased`}
+        className={`${figtree.variable} antialiased`}
       >
         {children}
+
+        {/* Umami Analytics Configuration
+              - afterInteractive so it loads it after page is interactive to prevent rendering block */}
+        <Script
+          src="/stats/script.js"
+          data-website-id="9aba93a1-34a2-49e3-97b6-578565ba53dc"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   );
